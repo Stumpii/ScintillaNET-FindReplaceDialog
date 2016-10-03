3ab427c63d6abfb943e7b46046f48b976e5e7317
James Tarkenton <james.tarkenton@gmail.com>
2016-10-03 17:24:46 -0400

    Fixing FindNext() and FindPrevious() on Replace Tab
    Fixed the FindNext() and FindPrevious() methods of the FindReplaceDialog
    class so that search text is synchronized across both tabs before
    searching. This allows them to be called via keyboard shortcuts (or some
    other method) while on the Replace tab and function as expected.

--- ScintillaNet FindReplaceDialog/FindReplace/FindReplaceDialog.cs
+++ ScintillaNet FindReplaceDialog/FindReplace/FindReplaceDialog.cs
@@ -516,6 +516,8 @@ namespace ScintillaNET_FindReplaceDialog
 
         public void FindNext()
         {
+            SyncSearchText();
+
             if (txtFindF.Text == string.Empty)
                 return;
 
@@ -555,6 +557,8 @@ namespace ScintillaNET_FindReplaceDialog
 
         public void FindPrevious()
         {
+            SyncSearchText();
+
             if (txtFindF.Text == string.Empty)
                 return;
 
@@ -822,6 +826,14 @@ namespace ScintillaNET_FindReplaceDialog
                 Hide();
 
             base.OnKeyDown(e);
+        }
+
+        private void SyncSearchText()
+        {
+            if (tabAll.SelectedTab == tpgFind)
+                txtFindR.Text = txtFindF.Text;
+            else
+                txtFindF.Text = txtFindR.Text;
         }
 
         private void AddFindMru()
